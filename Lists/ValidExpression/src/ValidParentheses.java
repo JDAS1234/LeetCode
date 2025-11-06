149695bff18f8503819f89c2d8959a888f306a38
JDAS1234 <julesgdasilva@gmail.com>
2025-11-06 15:30:56 -0500

    fixed ValidParentheses file working now

--- Lists/ValidExpression/src/ValidParentheses.java
+++ Lists/ValidExpression/src/ValidParentheses.java
@@ -1,7 +1,6 @@
 import java.util.Stack;
 
 public class ValidParentheses {
-
     public static void main(String[] args) {
         String s = "{[()]}";
         System.out.println(isBalanced(s)); // true
@@ -13,17 +12,17 @@ public class ValidParentheses {
         System.out.println(isBalanced(s)); // true
     }
 
-
     public static boolean isBalanced(String s) {
         Stack<Character> stack = new Stack<>();
         for (char ch : s.toCharArray()) {
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
-            } 
-            else if (ch == ')' || ch == '}' || ch == ']') {
-                if (stack.isEmpty()) return false;
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty())
+                    return false;
                 char top = stack.peek();
-                if (!isMatchingPair(top, ch)) return false;
+                if (!isMatchingPair(top, ch))
+                    return false;
                 stack.pop(); // removes the top bracket that matched
             }
 
@@ -31,15 +30,10 @@ public class ValidParentheses {
         return stack.isEmpty();
     }
 
-
     private static boolean isMatchingPair(char open, char close) {
         return (open == '(' && close == ')') ||
-               (open == '{' && close == '}') ||
-               (open == '[' && close == ']');
+                (open == '{' && close == '}') ||
+                (open == '[' && close == ']');
     }
-
-
-    
-
 
 }
