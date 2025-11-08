e9f5418375f5bbeb802aadd178d31dd55e0a7f63
JDAS1234 <julesgdasilva@gmail.com>
2025-11-08 15:31:52 -0500

    fixed warning

--- Static/Employees/src/Employees.java
+++ Static/Employees/src/Employees.java
@@ -2,7 +2,8 @@
 Make an employee class with a id (static) that increments everytime a new instance of employee is created  */
 public class Employees {
 
-    static int id = 0;    
+    static int id = 0; 
+       
 
     public Employees() {
         id++;
@@ -16,12 +17,10 @@ public class Employees {
 
     public static void main(String[] args) throws Exception {
         
-        
-        Employees e1 = new Employees();
-        Employees e2 = new Employees();
-        Employees e3 = new Employees(10);
-        Employees e4 = new Employees();
-
+        new Employees();
+        new Employees();
+        new Employees(10);
+        new Employees();
 
     }
 }
